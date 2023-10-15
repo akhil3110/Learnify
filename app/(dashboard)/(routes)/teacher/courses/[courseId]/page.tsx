d2309325549e2c81c73a11f97fb6c0c4d0cb220e
Akhil <akhil1659@gmail.com>
2023-10-15 14:55:41 +0530

    added edit tittle and edit description on Edit course page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,9 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm from "./_components/title-form";
+import DescriptionForm from "./_components/description-form";
+
 
 const CourseIdPage = async ({
     params
@@ -69,6 +72,14 @@ const CourseIdPage = async ({
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
+                    <DescriptionForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
