0e0332fc9f109fa89974602a0e463152512c9fb7
Akhil <akhil1659@gmail.com>
2023-10-16 03:01:18 +0530

    Configured Uploadthings and now user can upload course poster

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
+import ImageForm from "./_components/image-form";
 
 
 const CourseIdPage = async ({
@@ -78,6 +79,10 @@ const CourseIdPage = async ({
                     />
                     <DescriptionForm
                         initialData={course}
+                        courseId={course.id}
+                    />
+                    <ImageForm 
+                         initialData={course}
                         courseId={course.id}
                     />
                 </div>
