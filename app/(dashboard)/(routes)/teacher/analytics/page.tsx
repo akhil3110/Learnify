01f8048f3e7933338a0012c4a1854d067ecf8824
Akhil <akhil1659@gmail.com>
2023-11-03 01:50:29 +0530

    added Analytics page of teacher

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,41 @@
-const Analytics = () => {
+import { getAnalytics } from "@/actions/get-analytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import DataCard from "./_components/data-card";
+import Chart from "./_components/chart";
+
+const Analytics = async () => {
+
+    const {userId} = auth();
+
+    if(!userId) {
+        return redirect('/');
+    }
+
+    const {
+        data,
+        totalRevenue,
+        totalSales
+    } = await getAnalytics(userId);
+
+
+
     return ( 
-        <div className=" mt-20">
-            <h1>Teacher Analytics</h1>
+        <div className="p-6 ">
+            <div className=" grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+            <DataCard
+                    label="Total Revenue"
+                    value={totalRevenue}
+                    shouldFormat={true}
+                />
+                <DataCard
+                    label="Total Sales"
+                    value={totalSales}
+                />
+            </div>
+            <Chart
+                data={data}
+            />
         </div>
      );
 }
